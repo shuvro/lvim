0e88c5554b52b31398fd4a3ee7624e12fc30ec2a
shuvro <shuvro.dhrubo@gmail.com>
2023-02-20 15:43:24 +0600

    disable custom web devicons

--- lua/user/custom_user.lua
+++ lua/user/custom_user.lua
@@ -17,8 +17,8 @@ M.config = function()
     vim.opt.laststatus = 0
     vim.g.tpipeline_cursormoved = 1
   end
-  lvim.builtin.custom_web_devicons = true
-  lvim.use_icons = false -- only set to false if you know what are you doing
+  -- lvim.builtin.custom_web_devicons = true
+  lvim.use_icons = true -- only set to false if you know what are you doing
   lvim.builtin.sell_your_soul_to_devil = { active = true, prada = false }
   lvim.lsp.document_highlight = false
   lvim.builtin.task_runner = "async_tasks"
